1c1dc33b0e38df895db35e4363064c2ae260c949
dakinfemiwa <daveak1602@gmail.com>
2021-09-02 12:09:01 +0100

    Shows labels only when you click on the circles

--- scalarprojection.js
+++ scalarprojection.js
@@ -401,6 +401,10 @@ thetaInput.oninput = function() {
 }
 
 circle1.onmousedown = function(event) {
+    vRbracket1.style.display = "block";
+    vRbracket2.style.display = "block";
+    vRxlbl.style.display = "block";
+    vRylbl.style.display = "block";
     mousePressed = true;
     chosenV = 1;
     vectorGraph.style.cursor = "pointer";
@@ -409,6 +413,10 @@ circle1.onmousedown = function(event) {
 }
 
 circle1.onmouseup = function() {
+    vRbracket1.style.display = "none";
+    vRbracket2.style.display = "none";
+    vRxlbl.style.display = "none";
+    vRylbl.style.display = "none";
     mousePressed = false;
     chosenV = 0;
     
@@ -420,6 +428,10 @@ circle1.onmouseleave = function() {
 
 
 circle2.onmousedown = function(event) {
+    vSbracket1.style.display = "block";
+    vSbracket2.style.display = "block";
+    vSxlbl.style.display = "block";
+    vSylbl.style.display = "block";
     mousePressed = true;
     chosenV = 2;
     vectorGraph.style.cursor = "pointer";
@@ -428,6 +440,10 @@ circle2.onmousedown = function(event) {
 }
 
 circle2.onmouseup = function() {
+    vSbracket1.style.display = "none";
+    vSbracket2.style.display = "none";
+    vSxlbl.style.display = "none";
+    vSylbl.style.display = "none";
     mousePressed = false;
     chosenV = 0;
 }
@@ -460,6 +476,16 @@ vectorGraph.onmousemove = function(event) {
         updateVectorSVG();
     }
 }
+
+vRbracket1.style.display = "none";
+vRbracket2.style.display = "none";
+vRxlbl.style.display = "none";
+vRylbl.style.display = "none";
+
+vSbracket1.style.display = "none";
+vSbracket2.style.display = "none";
+vSxlbl.style.display = "none";
+vSylbl.style.display = "none";
 
 operate("r");
 updateVectorInput();
